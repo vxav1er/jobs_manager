0b917ede2b6ecff105621ed02b1c0e40238aafea
Vinicius Xavier <vxavier.braga@gmail.com>
2024-05-01 02:15:07 -0300

    feat: create Candidate (controller and entity) and add Lombok

---
+++ src/main/java/com/xavier/jobs_manager/modules/candidate/CandidateEntity.java
@@ -0,0 +1,17 @@
+package com.xavier.jobs_manager.modules.candidate;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+}
---
+++ src/main/java/com/xavier/jobs_manager/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,19 @@
+package com.xavier.jobs_manager.modules.candidate.controllers;
+
+
+import com.xavier.jobs_manager.modules.candidate.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        System.out.print("Candidate");
+        System.out.println(candidateEntity.getEmail());
+    }
+}
