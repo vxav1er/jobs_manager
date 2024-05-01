870b17b8fd5e15bd43595c9809e8073e4ba60c2f
Vinicius Xavier <vxavier.braga@gmail.com>
2024-05-01 01:28:19 -0300

    feat: created project structure

---
+++ src/main/java/com/xavier/jobs_manager/JobsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.jobs_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobsManagerApplication.class, args);
+    }
+
+}
