fb145a645110baa01c1fc47460af82c360de9dae
Vinicius Xavier <vxavier.braga@gmail.com>
2024-05-03 14:01:52 -0300

    feat: create candidates table

--- src/main/java/com/xavier/jobs_manager/modules/candidate/CandidateEntity.java
+++ src/main/java/com/xavier/jobs_manager/modules/candidate/CandidateEntity.java
@@ -1,16 +1,26 @@
 package com.xavier.jobs_manager.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@Entity(name = "candidates")
 public class CandidateEntity {
 
+    @Id()
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
     @Pattern(regexp = "\\s+", message = "The [username] field must not contain a space")
     private String username;
@@ -22,4 +32,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
