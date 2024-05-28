004a05010a78e82a5b96c8ee4aa4235547fedc08
Vinicius Xavier <vxavier.braga@gmail.com>
2024-05-27 21:15:42 -0300

    feat: add hash password on create candidate

--- src/main/java/com/xavier/jobs_manager/modules/candidate/services/CreateCandidateService.java
+++ src/main/java/com/xavier/jobs_manager/modules/candidate/services/CreateCandidateService.java
@@ -4,6 +4,7 @@ import com.xavier.jobs_manager.exeptions.UserFoundException;
 import com.xavier.jobs_manager.modules.candidate.CandidateEntity;
 import com.xavier.jobs_manager.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,17 @@ public class CreateCandidateService {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
